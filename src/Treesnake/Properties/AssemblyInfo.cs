2dca9a04affb1cee8fca8ed1cc422b3894d89ae5
Fawresin <fawresin@gmail.com>
2021-02-18 05:00:29 -0400

    Add project and solution files, editorconfig file, and source header file.

---
+++ src/Treesnake/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Treesnake")]
+[assembly: AssemblyDescription("Compiles Python scripts to LSL scripts")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Treesnake.Properties")]
+[assembly: AssemblyCopyright("Copyright © Fawresin 2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("19a22215-a81b-4ac0-bbfc-9e80bfefe6b3")]
